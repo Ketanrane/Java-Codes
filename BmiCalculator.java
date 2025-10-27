95ab796af0619e588eedbffbb53e73cf0f4b2763
Ketan Rane <raneketan1231@gmail.com>
2025-10-27 22:04:01 +0530

    Implement BMI category classification
    Added BMI category classification based on calculated BMI value.

--- BmiCalculator.java
+++ BmiCalculator.java
@@ -28,5 +28,18 @@ class BmiCalculator
         
         System.out.printf("BMI is "+ bmi);
 
+			 if (bmi < 18.5) {                               
+            System.out.println("Category: Underweight"); 
+        } else if (bmi < 24.9) {                        
+            System.out.println("Category: Normal weight");
+        } else if (bmi < 29.9) {                       
+            System.out.println("Category: Overweight");  
+        } else {                                        
+            System.out.println("Category: Obese");      
+        }
+
+        
+        sc.close();
 	}
 }
+
