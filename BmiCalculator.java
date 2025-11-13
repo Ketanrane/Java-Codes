8f2e719e8b23c1c5942eddfc17b9253b34b8ebad
Ketan Rane <raneketan1231@gmail.com>
2025-11-13 22:33:42 +0530

    Refactor BmiCalculator for improved readability

--- BmiCalculator.java
+++ BmiCalculator.java
@@ -9,26 +9,21 @@ class BmiCalculator
         double inchToMetre = 0.0254;
 
         Scanner sc = new Scanner(System.in);
-
-         
+			
         System.out.print("Enter weight in pounds: ");
         double weightInPounds = sc.nextDouble();
-        
         
         System.out.print("Enter height in inches: ");
         double heightInInches = sc.nextDouble();
 
-        
         double weightInKg = weightInPounds * poundToKg;
         double heightInMeters = heightInInches * inchToMetre;
 
         // Calculate BMI
         double bmi = weightInKg / (heightInMeters * heightInMeters);
-
-        
         System.out.printf("BMI is "+ bmi);
 
-			 if (bmi < 18.5) {                               
+		if (bmi < 18.5) {                               
             System.out.println("Category: Underweight"); 
         } else if (bmi < 24.9) {                        
             System.out.println("Category: Normal weight");
@@ -36,10 +31,9 @@ class BmiCalculator
             System.out.println("Category: Overweight");  
         } else {                                        
             System.out.println("Category: Obese");      
-        }
-
-        
+        } 
         sc.close();
 	}
 }
+
 
